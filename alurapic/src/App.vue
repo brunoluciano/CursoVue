b29f35e913ce402ec7f5167465ca781e1b1a2fdd
Bruno Luciano <bruno.lucian0544@gmail.com>
2020-06-29 15:56:56 -0300

    Aula 04 finalizada - Criando componentes e encapsulamento da view

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,16 +1,25 @@
+
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+  components: {
+    'meu-painel': Painel
+  },
+
   data() {
     return {
       titulo: 'Alurapic',
@@ -26,4 +35,25 @@ export default {
 </script>
 
 <style>
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
