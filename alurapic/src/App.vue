8b546f33ef475e82690c345cf40bc1204445b168
brunoluciano <bruno.lucian0544@gmail.com>
2020-06-29 23:20:33 -0300

    Curso Vue.js parte 1 finalizado

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,59 +1,47 @@
-
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
-        <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-        </meu-painel>
-      </li>
-    </ul>
+
+    <meu-menu :rotas="routes" />
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
   components: {
-    'meu-painel': Painel
+    'meu-menu': Menu
   },
 
   data() {
     return {
-      titulo: 'Alurapic',
-      fotos: []
+
+      routes
     }
-  },
-  created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
+
 };
 </script>
 
 <style>
-  .centralizado {
-    text-align: center;
-  }
-
   .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
   }
 
-  .lista-fotos {
-    list-style: none;
-  }
+  .pagina-enter, .pagina-leave-active {
+  height: 0;
+  opacity: 0
+}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
-  }
+  .pagina-enter-active, .pagina-leave-active {
+  transition: opacity .4s
+}
 </style>
